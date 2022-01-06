be639b2ef13199efa6d002401cd569907ee3f80c
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2022-01-06 17:57:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# this is to reconfigure wireshark
+wget https://raw.githubusercontent.com/billyball1517/dockerfile-x11docker-lxde/master/wireshark-expect && \
+chmod +x wireshark-expect && \
+./wireshark-expect && \
+rm -f ./wireshark-expect && \
+
 useradd -m -G wireshark,sudo -s /bin/bash kali
 
 service postgresql start
