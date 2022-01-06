e8606aee65606a6e89f37cf2ffdfdf926f8681f7
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2022-01-06 17:35:15 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Add local user
-# Either use the LOCAL_USER_ID if passed in at runtime or
-# fallback
-
-USER_ID=${LOCAL_USER_ID:-9001}
-GROUP_ID=${LOCAL_GROUP_ID:-9001}
-
-echo "Starting with UID : $USER_ID"
-adduser -D -u $USER_ID kali
-export HOME=/home/kali
-
-addgroup kali qemu
-
 service postgresql start
 
 exec /usr/bin/gosu kali "$@"
