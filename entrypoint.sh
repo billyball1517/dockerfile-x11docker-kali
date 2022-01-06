81977078e8aa89da860c05fa8d3da972f3ddb5da
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2022-01-06 18:00:35 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 export DEBIAN_FRONTEND=readline
 
 # this is to reconfigure wireshark
-wget https://raw.githubusercontent.com/billyball1517/dockerfile-x11docker-lxde/master/wireshark-expect && \
-chmod +x wireshark-expect && \
-./wireshark-expect && \
-rm -f ./wireshark-expect \
+wget https://raw.githubusercontent.com/billyball1517/dockerfile-x11docker-lxde/master/wireshark-expect
+chmod +x wireshark-expect
+./wireshark-expect
+rm -f ./wireshark-expect
 
 useradd -m -G wireshark,sudo -s /bin/bash kali
 
