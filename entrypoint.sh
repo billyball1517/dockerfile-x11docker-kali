a4d6abd81c30d6affdf4b8b8e1f52fd88c235230
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2022-05-10 20:17:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Either use the LOCAL_USER_ID if passed in at runtime or fallback
-
-USER_ID=${LOCAL_USER_ID:-9001}
-
-RUN useradd -u $USER_ID -m -s /bin/bash kali
-
-#echo "$KALI_PASS" | passwd --stdin kali
-
-addgroup kali sudo
-addgroup kali wireshark
-
 #exec /usr/sbin/gosu kali "$@"
