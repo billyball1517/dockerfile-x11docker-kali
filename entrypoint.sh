6116a3cdf9452d56db19795cc45280ff0f41f8a8
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2022-05-10 20:15:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 
 RUN useradd -u $USER_ID -m -s /bin/bash kali
 
-echo "$KALI_PASS" | passwd --stdin kali
+#echo "$KALI_PASS" | passwd --stdin kali
 
 addgroup kali sudo
 addgroup kali wireshark
